454c930cffffd8d41c1266190fd0b1a56cf0cdb7
Luke Wagner <69649333+LukeWagner89@users.noreply.github.com>
2020-12-10 11:35:06 -0800

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
-CLIENT_ID="0d67ee2500944a819efa1ce0df5e097c"
-CLIENT_SECRET="f12c8824003647ffabf5ded63b69ca80"
-USERNAME = "lucky.89"
-SCOPE = "playlist-modify-private"
-REDIRECT_URI = "http://localhost:8888/callback" #This is what I use but not the only one
-PLAYLIST_ID = "7s0O39hl9NPSqk3pcl95CM"
+CLIENT_ID="placeholder" #Get from spotify dev website
+CLIENT_SECRET="placeholder" #Get from spotify dev website
+USERNAME = "placeholder" #Your spotify username
+SCOPE = #Use "playlist-modify-public" for public playlists and "playlist-modify-private" for private ones
+REDIRECT_URI = "http://localhost:8888/callback" #This is what I use but not the only option
+PLAYLIST_ID = "placeholder" #Get from playlist options > Share > Copy Spotify URI & remove "spotify:playlist:"
