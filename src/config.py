1b7834174a2e49cf1432dad4d168776f77ae41fb
Luke Wagner <69649333+LukeWagner89@users.noreply.github.com>
2020-12-10 11:32:08 -0800

    Add files via upload

---
+++ src/config.py
@@ -0,0 +1,6 @@
+CLIENT_ID="0d67ee2500944a819efa1ce0df5e097c"
+CLIENT_SECRET="f12c8824003647ffabf5ded63b69ca80"
+USERNAME = "lucky.89"
+SCOPE = "playlist-modify-private"
+REDIRECT_URI = "http://localhost:8888/callback" #This is what I use but not the only one
+PLAYLIST_ID = "7s0O39hl9NPSqk3pcl95CM"
