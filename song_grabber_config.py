d5201808bb0009d22f34ea3078006b33ad96f527
Luke Wagner <69649333+LukeWagner89@users.noreply.github.com>
2020-12-09 22:52:42 -0800

    Add files via upload

---
+++ song_grabber_config.py
@@ -0,0 +1,6 @@
+CLIENT_ID="placeholder"
+CLIENT_SECRET="placeholder"
+USERNAME = "placeholder"
+SCOPE = "placeholder" #Use "playlist-modify-public" for public playlists and "playlist-modify-private" for private playlists.
+REDIRECT_URI = "http://localhost:8888/callback" #This is what I use but not the only one
+PLAYLIST_ID = "placeholder"
